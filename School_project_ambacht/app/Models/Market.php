6babd57d6d955cf35df1deb11a3ba827c993f40e
CJRadd <chaniscoj@gmail.com>
2021-03-08 11:45:33 +0100

    Models

--- School_project_ambacht/app/Models/Market.php
+++ School_project_ambacht/app/Models/Market.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Market extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'label',
+        'location',
+        'photo',
+        'description',
+    ];
+
+    public function products(){
+        return $this->HasMany(Product::class);
+    }
 }
