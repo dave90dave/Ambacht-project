4561ec3450b91b840d767c86c51cef15b35d550e
dave90dave <dave.hulst@gmail.com>
2021-03-16 16:16:52 +0100

    Login required for markets and products (not best practice for production)

--- School_project_ambacht/app/Http/Controllers/ManagementController.php
+++ School_project_ambacht/app/Http/Controllers/ManagementController.php
@@ -11,17 +11,26 @@ class ManagementController extends Controller
 {
     public function products()
     {
+    if (Auth::id() == null) {
+        abort(401);
+    } else {
         $products = Product::all();
 
         return view("management.products", compact("products"));
     }
+}
 
     public function markets()
     {
+        if (Auth::id() == null) {
+            abort(401);
+        } else {
         $loggedInUserId = Auth::id();
         $markets = Market::all()->where("user_id", "=", $loggedInUserId);
 
-
         return view("management.markets", compact("markets"));
+        }
+
+
     }
 }
