513b817dd31a6ba1c2749869dfff4b8ecc6c1cca
dave90dave <dave.hulst@gmail.com>
2021-03-30 12:01:07 +0200

    dropdown werkt weer

--- School_project_ambacht/resources/views/layouts/app.blade.php
+++ School_project_ambacht/resources/views/layouts/app.blade.php
@@ -65,7 +65,7 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" data-bs-toggle="dropdown" aria-expanded="false">
                                     {{ Auth::user()->name }}
                                 </a>
 
