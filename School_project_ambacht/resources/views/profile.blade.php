8a4feabbe632112d2cf58619361d2f59646d4d54
dave90dave <dave.hulst@gmail.com>
2021-04-28 17:11:11 +0200

    Kleine aanpassingen aan profiel pagina

--- School_project_ambacht/resources/views/profile.blade.php
+++ School_project_ambacht/resources/views/profile.blade.php
@@ -11,12 +11,12 @@
         <div class="row row-cols-1 row-cols-md-1 g-1">
             <div class="col">
 
-                <img src="/resources/img/test/profile.jpg" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">{{$name}}</h5>
-                    <p class="card-text">Heeft X markten, met in totaal X producten</p>
-                    <small class="text-muted">{{$createdAt}}</small>
+                <img src="/resources/img/test/profile.jpg" height="100px" alt="...">
+                <h5 class="card-title">{{$name}}</h5>
+                <small class="text-muted">Gebruiker is actief sinds: {{$createdAt}}</small>
 
+                <p class="card-text">Hier komen alle markets van de gebruiker</p>
+                <p class="card-text">Hier komen alle producten van de gebruiker</p>
             </div>
         </div>
     </div>
