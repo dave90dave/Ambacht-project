f966906300226e58594c5a41fd8d85f0040ef2dd
dave90dave <dave.hulst@gmail.com>
2021-05-17 11:28:49 +0200

    fixed namespace error

--- School_project_ambacht/app/Http/Controllers/Admin/DashboardController.php
+++ School_project_ambacht/app/Http/Controllers/Admin/DashboardController.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\Market;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class AdminDashboard extends Controller
+class DashboardController extends Controller
 {
     public function index()
     {
