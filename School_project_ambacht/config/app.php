a9068578ca3b1e757c5c3306dfca479579535ec2
dave90dave <dave.hulst@gmail.com>
2021-06-07 16:56:56 +0200

    Installed googlmapper

--- School_project_ambacht/config/app.php
+++ School_project_ambacht/config/app.php
@@ -175,6 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Cornford\Googlmapper\MapperServiceProvider::class,
+
     ],
 
     /*
@@ -226,6 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Mapper' => Cornford\Googlmapper\Facades\MapperFacade::class,
 
     ],
 
